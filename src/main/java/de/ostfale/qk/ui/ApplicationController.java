d01866e35659fb9ee8a9fb5909f4a365891ccc4c
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2025-02-08 18:22:25 +0100

    Refactor dashboard to app-centric structure.
    Replaces "dashboard-view" with "app-view" to enable a broader, modular application layout. Introduces navigation buttons for multiple views and an exit option, updating the controller and styles accordingly.

---
+++ src/main/java/de/ostfale/qk/ui/ApplicationController.java
@@ -0,0 +1,62 @@
+package de.ostfale.qk.ui;
+
+import io.quarkiverse.fx.views.FxView;
+import jakarta.enterprise.context.Dependent;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import org.jboss.logging.Logger;
+
+@FxView("app-view")
+@Dependent
+public class ApplicationController {
+
+    private static final Logger log = Logger.getLogger(ApplicationController.class);
+
+    @FXML
+    Parent root;
+
+    @FXML
+    public void initialize() {
+        Stage stage = new Stage();
+        Scene scene = new Scene(this.root);
+        stage.setScene(scene);
+        stage.setTitle("Badminton Statistics");
+        stage.getIcons().add(new Image("images/shuttle_logo.jpg"));
+        log.info("Dashboard initialized");
+        stage.show();
+    }
+
+    @FXML
+    void showDashboardView(ActionEvent event) {
+        log.info("Show Dashboard View");
+    }
+
+    @FXML
+    void showRankingView(ActionEvent event) {
+        log.info("Show Ranking View");
+    }
+
+    @FXML
+    void showStatisticsView(ActionEvent event) {
+        log.info("Show Ranking View");
+    }
+
+    @FXML
+    void showTournaments(ActionEvent event) {
+        log.info("Show Tournaments View");
+    }
+
+    @FXML
+    void showFavorites(ActionEvent event) {
+        log.info("Show Favorites View");
+    }
+
+    @FXML
+    void closeApplication(ActionEvent event) {
+        System.exit(0);
+    }
+}
